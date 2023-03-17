e0fcd52f1131c3c517811763e294b03d8d51e198
luis <luisnavarroFS@gmail.com>
2023-03-17 21:05:33 +0300

    config authentication

---
+++ config/utils.js
@@ -0,0 +1,32 @@
+const jsonwebtoken = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const pathToKey = path.join(__dirname, 'id_rsa_priv.pem');
+const PRIV_KEY = fs.readFileSync(pathToKey, 'utf8');
+
+function issueJWT(user) {
+  const _id = user._id;
+
+  const expiresIn = "1m";
+
+  const payload = {
+    sub: _id,
+    iat: Date.now()
+  };
+
+  const signedToken = jsonwebtoken.sign(payload, PRIV_KEY, { expiresIn: expiresIn, algorithm: 'RS256' });
+
+  return {
+    token: "Bearer " + signedToken,
+    expires: expiresIn
+  }
+}
+
+function validPassword(password, hash, salt) {
+  var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+  return hash === hashVerify;
+}
+
+module.exports.issueJWT = issueJWT;
+module.exports.validPassword = validPassword
