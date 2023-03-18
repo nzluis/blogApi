6e40ecc992bf7683a220a157a267a58f8fc623f9
luis <luisnavarroFS@gmail.com>
2023-03-18 19:18:43 +0300

    update type model

--- models/post.js
+++ models/post.js
@@ -4,14 +4,14 @@ const { DateTime } = require("luxon");
 
 const PostSchema = new Schema({
     title: {type: String, required: true},
-    author: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
     text: {type: String, required: true},
     status: {type: String, enum: ['Show', 'Hide'],default: 'Hide', required: true}, 
     timestamp: {type: Date, default: Date.now, required: true},
 })
 
 PostSchema.virtual('url').get(function(){
-    return `/posts/${this._id}`
+    return `/post/${this._id}`
 })
 
 PostSchema.virtual("timestamp_formatted").get(function(){
