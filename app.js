4e04232bd3cbfc778eb534b88894c368bec78489
luis <luisnavarroFS@gmail.com>
2023-03-20 16:16:47 +0300

    del public

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter)
 
